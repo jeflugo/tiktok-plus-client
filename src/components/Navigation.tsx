c736f95a9277808c317e623fdedfa9c3473baed6
jeflugo <jefersoneduardoluego@gmail.com>
2024-07-27 10:51:12 -0400

    styles for suggested search

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import { IconType } from 'react-icons'
 import { AiFillHome } from 'react-icons/ai'
-import { BiMessageAlt, BiUser } from 'react-icons/bi'
+import { BiChevronRight, BiMessageAlt, BiUser } from 'react-icons/bi'
 import { BsSearchHeartFill } from 'react-icons/bs'
+import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 type TLink = {
@@ -39,27 +41,41 @@ const NAVIGATION_LINKS: TLink[] = [
 ]
 
 export default function Navigation() {
+	const [search] = useState(true)
 	return (
-		<div className='border-t-2 border-white bg-black text-white py-2'>
-			<div className='flex justify-evenly items-center'>
-				{NAVIGATION_LINKS.map(({ name, Icon, link }, index) => {
-					if (index === 2)
+		<div>
+			{search && (
+				<div className='bg-black/95 px-2 py-1 text-white'>
+					<div className='flex justify-between items-center'>
+						<div className='flex gap-1 items-center'>
+							<FaSearch />
+							<span>Search - asdasd</span>
+						</div>
+						<BiChevronRight />
+					</div>
+				</div>
+			)}
+			<div className='border-t-2 border-white bg-black text-white py-2'>
+				<div className='flex justify-evenly items-center'>
+					{NAVIGATION_LINKS.map(({ name, Icon, link }, index) => {
+						if (index === 2)
+							return (
+								<div>
+									<Link to={link}>
+										<Icon />
+									</Link>
+								</div>
+							)
 						return (
-							<div>
+							<div className='flex flex-col justify-center items-center'>
 								<Link to={link}>
 									<Icon />
 								</Link>
+								<h4>{name}</h4>
 							</div>
 						)
-					return (
-						<div className='flex flex-col justify-center items-center'>
-							<Link to={link}>
-								<Icon />
-							</Link>
-							<h4>{name}</h4>
-						</div>
-					)
-				})}
+					})}
+				</div>
 			</div>
 		</div>
 	)
