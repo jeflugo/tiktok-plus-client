983ab17c5f153ff5959e09c11538b9c323bf8816
jeflugo <jefersoneduardoluego@gmail.com>
2024-07-29 09:24:55 -0400

    change icons

--- src/pages/Home/pages/Vids/components/SideBar.tsx
+++ src/pages/Home/pages/Vids/components/SideBar.tsx
@@ -1,15 +1,15 @@
-import { BiHeart, BiTag } from 'react-icons/bi'
-import { FaRegCommentDots } from 'react-icons/fa6'
-import { RiShareForwardLine } from 'react-icons/ri'
+import { AiFillMessage } from 'react-icons/ai'
+import { FaHeart, FaStar } from 'react-icons/fa'
+import { RiShareForwardFill } from 'react-icons/ri'
 
 export default function SideBar() {
 	return (
 		<div className='absolute	bottom-2 right-2 flex flex-col gap-2 justify-center items-center'>
 			<div className='w-8 h-8 border-2 border-white rounded-full'></div>
-			<BiHeart size={27} />
-			<FaRegCommentDots size={27} />
-			<BiTag size={27} />
-			<RiShareForwardLine size={27} />
+			<FaHeart size={25} />
+			<AiFillMessage size={25} />
+			<FaStar size={25} />
+			<RiShareForwardFill size={25} />
 			<div className='w-6 h-6 border-2 border-white rounded-full'></div>
 		</div>
 	)
